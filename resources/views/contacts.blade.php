49d5db04356b9073051e1f831d5edf05097b4a26
Trestrantara <trestantaraweilga@gmail.com>
2022-06-07 17:07:52 +0700

    Modul 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,19 +4,20 @@
 <div class="title-top mb-5">
     <h2 class="fw-blod">Contacts Us</h2>
 </div>
-<form>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
 <div class="form-floating mb-3">
-  <input type="text" class="form-control" id="inputname" placeholder="Nama">
+  <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
   <label for="floatingInput">Nama</label>
 </div>
 <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="inputemail" placeholder="Email">
+  <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email">
   <label for="inputemail" class="form-label">Email</label>
 </div>
   <div class="form-floating mb-3">
-  <textarea class="form-control" placeholder="Tinggalkan pesan disini" id="floatingTextarea" style="height: 100px"></textarea>
+  <textarea class="form-control" placeholder="Tinggalkan pesan disini" id="floatingTextarea" style="height: 100px" name="pesan"></textarea>
   <label for="floatingTextarea">Pesan</label>
 </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Kirim</button>
 </form>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,13 +11,14 @@
     <title>Wielga | {{ $title }}</title>
   </head>
   <body>
-    @include('partials.navbar');
 
-       <div class="container mt-4">
-          @yield('container')
-       </div>
+    @include('partials.navbar')
+
+  <div class="container mt-4">
+     @yield('container')
+  </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
-</html>S
+</html> 
